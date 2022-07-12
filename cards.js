73c06f7eb26fbdf956801106ca51b474637bd66d
Владислав <102624648+vladsatan@users.noreply.github.com>
2022-07-12 15:40:57 +0300

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,10 @@
+const cardsInfo = [
+    {id: 1,photo: './img/card-photo1.png',title: 'LUX* Belle Mare',location: '1749 Wheeler Ridge  Delaware',guests: '2 x Guests',room: '1 x Room',pastPrise: '$ 8,500',newprise: '$ 3,000'},
+    {id: 2,photo: './img/card-photo 2.png',title: 'White Palace',location: '1749 Wheeler Ridge  Delaware',guests: '2 x Guests',room: '1 x Room',pastPrise: '$ 10,500',newprise: '$ 9,500'},
+    {id: 3,photo: './img/card-photo3.png',title: 'Luxury Place',location: '1749 Wheeler Ridge  Delaware',guests: '2 x Guests',room: '1 x Room',pastPrise: '$ 4,500',newprise: '$ 2,500'},
+    {id: 4,photo: './img/card-photo4.png',title: 'Hotel Five Star',location: '1749 Wheeler Ridge  Delaware',guests: '2 x Guests',room: '1 x Room',pastPrise: '$ 6,500',newprise: '$ 3,500'},
+    {id: 5,photo: './img/card-photo5.png',title: 'Hotel Blue Haven',location: '1749 Wheeler Ridge  Delaware',guests: '2 x Guests',room: '1 x Room',pastPrise: '$ 10,500',newprise: '$ 8,500'},
+    {id: 6,photo: './img/card-photo6.png',title: 'Grand Hilton Hotel',location: '1749 Wheeler Ridge  Delaware',guests: '2 x Guests',room: '1 x Room',pastPrise: '$ 11 500',newprise: '$ 8,500'}
+]
+
+export default cardsInfo;
